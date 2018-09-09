fbe0069861ce257dac1fd7fec6963834d0d00c22
phytomosaic <smithr2@oregonstate.edu>
2018-09-09 12:00:41 -0700

    Add procrustes/fourthcorner/rlq

--- R/all_approaches_09Sep2018.R
+++ R/all_approaches_09Sep2018.R
@@ -6,6 +6,7 @@
 require(ade4)
 require(ecole)
 require(vegan)
+require(mgcv)
 
 ### data load
 data(mafragh)
@@ -24,8 +25,8 @@ rownames(tra)[rownames(tra)== 'alisma_plantago_aquatica'] <-
 
 ### split species/traits into two (to simulate 'plant vs lichen')
 ###    (break is at deepest phylogenetic node)
-i1 <- 1:22
-i2 <- 23:56
+i1   <- 1:22
+i2   <- 23:56
 spe1 <- mx_dropzero(spe[,i1])
 spe2 <- mx_dropzero(spe[,i2])
 tra1 <- tra[rownames(tra) %in% names(spe1),]
@@ -34,8 +35,8 @@ tra2 <- tra[rownames(tra) %in% names(spe2),]
 ### zero-adjustment for empty SUs
 spe1 <- cbind(spe1, dummy=rep(1, nrow(spe1)))
 spe2 <- cbind(spe2, dummy=rep(1, nrow(spe2)))
-tra1 <- rbind(tra1, dummy=rep(1, ncol(tra1)))
-tra2 <- rbind(tra2, dummy=rep(1, ncol(tra2)))
+tra1 <- rbind(tra1, dummy=colMeans(tra))
+tra2 <- rbind(tra2, dummy=colMeans(tra))
 
 ### transformations
 spe1 <- data.frame(genlogtrans(spe1))
@@ -46,7 +47,7 @@ tra2 <- data.frame(decostand(tra2, 'range'))
 cwm1 <- data.frame(makecwm(spe1, tra1))
 cwm2 <- data.frame(makecwm(spe2, tra2))
 
-### ordinations
+### NMDS ordinations
 `ordfn` <- function(comm, distance, k, ...){
      metaMDS(comm=comm, distance=distance, k=k, try=100, trymax=200,
              autotransform=F, trace=0, maxit=500, weakties=TRUE)
@@ -64,8 +65,6 @@ plot(m4)
 ### procrustes
 p1 <- protest(m1,m2) # plant vs lichen species
 p2 <- protest(m3,m4) # plant vs lichen CWM traits
-
-### procrustes correlations
 p1$t0  # plant vs lichen species
 p2$t0  # plant vs lichen CWM traits
 
@@ -98,7 +97,36 @@ p2$t0  # plant vs lichen CWM traits
 # envfit(m1, env, perm=999)    # LINEAR correlations
 gamfit(m1, env)  # lichen species fit to env
 gamfit(m2, env)  #  plant species fit to env
-gamfit(m3, env)  # lichen traits fit to env
-gamfit(m4, env)  #  plant traits fit to env
+gamfit(m3, env)  # lichen traits CWM fit to env
+gamfit(m4, env)  #  plant traits CWM fit to env
+### TODO: need coln permutation tests...
 
-###   END   #########################################################
+### fourthcorner
+(f1 <- fourthcorner(env, spe1, tra1, nrepet=999, modeltype=6))
+(f2 <- fourthcorner(env, spe2, tra2, nrepet=999, modeltype=6))
+
+### RLQ and fourthcorner.rlq
+`rlqfn` <- function(spe, env, tra, ndim=2, ...){
+     ca     <-       dudi.coa(spe, scannf=F, nf=ndim)
+     pc_env <- dudi.hillsmith(env, scannf=F, nf=ndim, row.w=ca$lw)
+     pc_tra <- dudi.hillsmith(tra, scannf=F, nf=ndim, row.w=ca$cw)
+     out    <- rlq(pc_env, ca, pc_tra, scannf=F, nf=ndim)
+     out
+}
+`fc_envfit` <- function(x, ...){
+     if(!class(x)[1] == 'rlq') stop('must be class `rlq`')
+     fc   <- fourthcorner.rlq(x, type='R.axes')
+     nm   <- fc$colnames.R
+     nenv <- length(nm)
+     tt   <- cbind(r_obsvd  = fc$tabD2$obs,
+                   adj_pval = fc$tabD2$adj.pvalue)
+     out  <- cbind(tt[1:nenv,], tt[(nenv+1):(nenv*2),])
+     dimnames(out)[[1]] <- nm
+     out
+}
+r1 <- rlqfn(spe1, env, tra1)
+r2 <- rlqfn(spe2, env, tra2)
+fc_envfit(r1)
+fc_envfit(r2)
+
+###   END   ########################################################
