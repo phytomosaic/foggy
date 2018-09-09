352fccdc413bc4c689758fceb84c5e0b9a134a3c
phytomosaic <smithr2@oregonstate.edu>
2018-09-09 10:58:28 -0700

    Add procrustes procedure

---
+++ R/approach_procrustes.R
@@ -0,0 +1,104 @@
+######################################################################
+# Procrustes/nonlinear gradient approach
+#  Rob Smith, smithr2@oregonstate.edu, Oregon State Univ, 09 Sep 2018
+## CC-BY-SA 4.0 License (Creative Commons Attribution-ShareAlike 4.0)
+
+require(ade4)
+require(ecole)
+require(vegan)
+
+### data load
+data(mafragh)
+d <- mafragh
+xy  <- d$xy
+env <- d$env
+colnames(env) <- ecole::clean_text(colnames(env), lower=TRUE)
+spe <- d$flo
+colnames(spe) <- ecole::clean_text(d$spenames$scientific, lower=TRUE)
+tra <- d$traits
+tra <- data.frame(tra[[1]],tra[[2]],tra[[3]],tra[[4]])
+colnames(tra) <- ecole::clean_text(colnames(tra), lower=TRUE)
+tra <- tra[,!names(tra)%in%c('seasonnal','succulence')]
+rownames(tra)[rownames(tra)== 'alisma_plantago_aquatica'] <-
+     'alisma_plantago'
+
+### split species/traits into two (to simulate 'plant vs lichen')
+###    (break is at deepest phylogenetic node)
+i1 <- 1:22
+i2 <- 23:56
+spe1 <- mx_dropzero(spe[,i1])
+spe2 <- mx_dropzero(spe[,i2])
+tra1 <- tra[rownames(tra) %in% names(spe1),]
+tra2 <- tra[rownames(tra) %in% names(spe2),]
+
+### zero-adjustment for empty SUs
+spe1 <- cbind(spe1, dummy=rep(1, nrow(spe1)))
+spe2 <- cbind(spe2, dummy=rep(1, nrow(spe2)))
+tra1 <- rbind(tra1, dummy=rep(1, ncol(tra1)))
+tra2 <- rbind(tra2, dummy=rep(1, ncol(tra2)))
+
+### transformations
+spe1 <- data.frame(genlogtrans(spe1))
+spe2 <- data.frame(genlogtrans(spe2))
+env  <- data.frame(decostand(scale(env, center=F), 'range'))
+tra1 <- data.frame(decostand(tra1, 'range'))
+tra2 <- data.frame(decostand(tra2, 'range'))
+cwm1 <- data.frame(makecwm(spe1, tra1))
+cwm2 <- data.frame(makecwm(spe2, tra2))
+
+### ordinations
+`ordfn` <- function(comm, distance, k, ...){
+     metaMDS(comm=comm, distance=distance, k=k, try=100, trymax=200,
+             autotransform=F, trace=0, maxit=500, weakties=TRUE)
+}
+m1 <- ordfn(spe1, 'bray', 2)
+m2 <- ordfn(spe2, 'bray', 2)
+m3 <- ordfn(cwm1, 'altGower', 2)
+m4 <- ordfn(cwm2, 'altGower', 2)
+set_par(4)
+plot(m1)
+plot(m2)
+plot(m3)
+plot(m4)
+
+### procrustes
+p1 <- protest(m1,m2) # plant vs lichen species
+p2 <- protest(m3,m4) # plant vs lichen CWM traits
+
+### procrustes correlations
+p1$t0  # plant vs lichen species
+p2$t0  # plant vs lichen CWM traits
+
+### fit GAMs to regress each enviro variable on NMDS scores
+`gamfit` <- function(ord, env, ...){
+     require(mgcv)
+     ndim <- ord$ndim
+     nm   <- dimnames(env)[[2]]
+     nenv <- length(nm)
+     scr  <- scores(ord)
+     stopifnot(identical(rownames(scr), rownames(env)))
+     out <- matrix(NA, nrow=nenv, ncol=ndim+1)
+     dimnames(out)[[1]] <- nm
+     dimnames(out)[[2]] <- c(dimnames(scr)[[2]],'Adj-R2')
+     xnam <- rep(NA, ndim)
+     for(i in 1:ndim){
+          xnam[i] <- paste0('s(scr[,',i,'])')
+     }
+     right <- paste(xnam, collapse= '+')
+     for(i in 1:nenv){
+          left <- paste0('env[,',i,'] ~ ')
+          fmla <- as.formula(paste(left, right))
+          m  <- gam(fmla)
+          ss <- summary(m)
+          out[i,1:ndim] <- as.numeric(sprintf('%.3f', round(ss$s.pv,3)))
+          out[i,ndim+1] <- as.numeric(sprintf('%.3f', round(ss$r.sq,3)))
+     }
+     out
+}
+# envfit(m1, env, perm=999)    # LINEAR correlations
+gamfit(m1, env)  # lichen species fit to env
+gamfit(m2, env)  #  plant species fit to env
+gamfit(m3, env)  # lichen traits fit to env
+gamfit(m4, env)  #  plant traits fit to env
+
+###   END   #########################################################
