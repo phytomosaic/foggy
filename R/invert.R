6def0c8000afff0975d898cff043b9f5d4607b6f
phytomosaic <smithr2@oregonstate.edu>
2018-09-10 14:08:07 -0700

    Update docs for two clean datasets

---
+++ R/invert.R
@@ -0,0 +1,30 @@
+#' @name invert
+#' @title Macroinvertebrates of Loire River data
+#' @aliases invert
+#' @docType data
+#' @description
+#' This data set gives information about invertebrate species,
+#'     environment, traits, and phylogeny.
+#'
+#' @format A list containing:\cr
+#'     - \code{spe} 38 observations of 40 invertebrate species,\cr
+#'     - \code{env} 38 observations of 3 aquatic enviro variables,\cr
+#'     - \code{tra} 11 traits for 40 species,\cr
+#'     - \code{phy} phylogeny for 40 species.
+#'
+#' @details
+#' See \code{?macroloire} in \code{ade4}.
+#'
+#' @source Ivol et al. (1997).
+#'
+#' @references
+#' Ivol, J. M., Guinand, B., Richoux, P. and Tachet, H. 1997.
+#'      Longitudinal changes in Trichoptera and Coleoptera assemblages
+#'      and environmental conditions in the Loire River (France).
+#'      Archiv for Hydrobiologie 138: 525–557.
+#'
+#' @examples
+#' \dontrun{?macroloire} # in package 'ade4'
+#'
+#' @keywords datasets
+"invert"
---
+++ R/veg.R
@@ -0,0 +1,32 @@
+#' @name veg
+#' @title Mafragh, Algeria vegetation data
+#' @aliases veg
+#' @docType data
+#' @description
+#' This data set gives information about spatial coordinates, species,
+#'     environment, traits, and phylogeny.
+#'
+#' @format A list containing:\cr
+#'     - \code{xy}  97 observations of 2 spatial coordinates,\cr
+#'     - \code{spe} 97 observations of 56 plant species,\cr
+#'     - \code{env} 347 observations of 11 soil enviro variables,\cr
+#'     - \code{tra} 12 traits for 56 plant species,\cr
+#'     - \code{phy} phylogeny for 56 plant species.
+#'
+#' @details
+#' See \code{?mafragh} in \code{ade4}.
+#'
+#' @source Pavoine et al. (2011).
+#'
+#' @references
+#' Pavoine, S., Vela, E., Gachet, S., de Bélair, G. and Bonsall, M. B.
+#'      2011. Linking patterns in phylogeny, traits, abiotic variables
+#'      and space: a novel approach to linking environmental filtering
+#'      and plant community assembly. Journal of Ecology 99:165–175.
+#'      doi:10.1111/j.1365-2745.2010.01743.x
+#'
+#' @examples
+#' \dontrun{?mafragh} # in package 'ade4'
+#'
+#' @keywords datasets
+"veg"
